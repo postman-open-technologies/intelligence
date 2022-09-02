1a005945b4fd6eb670129a7b1bb2c70bba99ffd2
Kin Lane <kinlane@gmail.com>
2022-09-02 10:54:23 -0700

    push template

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# intelligence
-This is a repository to manage intelligence work for the platform.
+# Intelligence
+This is a repository for managing intelligence work via the Postman platform, using [a workspace](https://postman.postman.co/workspace/Postman-Open-Technologies---Int~5153d00c-5feb-4d2c-b184-a65479f14e6b/overview) to automate how the show works.
+
+If you have any qeustions you can leave an issue or email kin.lane@postman.com for more information.
---
+++ tasks/get/index.js
@@ -0,0 +1,54 @@
+const vandium = require('vandium');
+const mysql  = require('mysql');
+
+exports.handler = vandium.generic()
+  .handler( (event, context, callback) => {
+
+    var connection = mysql.createConnection({
+    host     : process.env.host,
+    user     : process.env.user,
+    password : process.env.password,
+    database : process.env.database
+    });
+    
+    var search = 0;
+    if(event.search){
+      search = event.search;
+    }    
+    
+    var tags = 0;
+    if(event.tags){
+      tags = event.tags;
+    }   
+    
+    var role = 0;
+    if(event.role){
+      role = event.role;
+    }       
+    
+    var page = 0;
+    if(event.page){
+      page = event.page;
+    }
+    
+    var limit = 5;
+    if(event.limit){
+      limit = event.limit;
+    }   
+    if(limit > 25){
+      limit = 25;
+    }
+
+    var sql = "SELECT * FROM tasks t WHERE id IS NOT NULL";
+    if(search != ''){
+       sql += " AND (e.title LIKE '%" + search + "%' OR e.description LIKE '%" + search + "%')";
+    }   
+    sql += " ORDER BY title";
+    sql += " LIMIT " + page + "," + limit;
+    connection.query(sql, function (error, results, fields) {
+
+    callback( null, results );
+
+  });
+  connection.end();
+});
